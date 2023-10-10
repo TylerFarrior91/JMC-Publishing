a22de0fc880bd22404d66ea84cc4ca5925fd5b4b
Tyler Farrior <tylerfarrior@gmail.com>
2023-10-10 17:09:32 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// Create email and Facebook links
+// const emailLink = document.createElement("a");
+// emailLink.href = "mailto:author@example.com";
+// emailLink.textContent = "Email";
+// document.querySelector(".contact-links").appendChild(emailLink);
+
+// const facebookLink = document.createElement("a");
+// facebookLink.href = "https://www.facebook.com/authorprofile";
+// facebookLink.textContent = "Facebook";
+// document.querySelector(".contact-links").appendChild(facebookLink);
+
+// Create raindrops using JavaScript and add them to the body
+// function createRaindrops() {
+//   const numberOfRaindrops = 100;
+//   const body = document.querySelector("body");
+
+//   for (let i = 0; i < numberOfRaindrops; i++) {
+//     const raindrop = document.createElement("div");
+//     raindrop.className = "raindrop";
+//     raindrop.style.left = Math.random() * 100 + "vw";
+//     raindrop.style.animationDuration = Math.random() * 2 + 1 + "s";
+//     body.appendChild(raindrop);
+//   }
+// }
+
+// // Call the function to create raindrops
+// createRaindrops();
