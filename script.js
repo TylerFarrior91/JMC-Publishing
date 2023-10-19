28ab9c66e562471a8a40926b0f8e3469a45a0892
Tyler Farrior <tylerfarrior@gmail.com>
2023-10-18 22:14:17 -0400

    switching js coodes

--- script.js
+++ script.js
@@ -10,18 +10,18 @@
 // document.querySelector(".contact-links").appendChild(facebookLink);
 
 //Create raindrops using JavaScript and add them to the body
-function createRaindrops() {
-  const numberOfRaindrops = 100;
-  const body = document.querySelector("body");
+//function createRaindrops() {
+  //const numberOfRaindrops = 100;
+  //const body = document.querySelector("body");
 
-  for (let i = 0; i < numberOfRaindrops; i++) {
-    const raindrop = document.createElement("div");
-    raindrop.className = "raindrop";
-    raindrop.style.left = Math.random() * 100 + "vw";
-    raindrop.style.animationDuration = Math.random() * 2 + 1 + "s";
-    body.appendChild(raindrop);
-  }
-}
+  //for (let i = 0; i < numberOfRaindrops; i++) {
+    //const raindrop = document.createElement("div");
+    //raindrop.className = "raindrop";
+    //raindrop.style.left = Math.random() * 100 + "vw";
+    //raindrop.style.animationDuration = Math.random() * 2 + 1 + "s";
+    //body.appendChild(raindrop);
+  //}
+//}
 
 // Call the function to create raindrops
-createRaindrops();
+//createRaindrops();
